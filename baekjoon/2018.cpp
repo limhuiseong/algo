4c8989b9fe2461bd3cd2eb5c23d9f700921bd253
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2024-06-29 07:34:14 +0000

    Refactor: Improve code quality

--- baekjoon/2018.cpp
+++ baekjoon/2018.cpp
@@ -2,32 +2,38 @@
 
 using namespace std;
 
-int main() {
-	int N;
+int N;
+int cnt = 1;
+int front_idx = 1;
+int end_idx = 1;
+int sum = 1;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
 	cin >> N;
-	
-	int cnt = 1;
-	int start_idx = 1;
-	int end_idx = 1;
-	int sum = 1;
-	
-	while (end_idx != N) {
-		if (sum == N) {
+
+	while (end_idx != N)
+	{
+		if (sum == N)
+		{
 			cnt++;
-			end_idx++;
-			sum += end_idx;
+			sum += ++end_idx;
 		}
-		else if (sum > N) {
-			sum -= start_idx;
-			start_idx++;
+		else if (sum > N)
+		{
+			sum -= front_idx++;
 		}
-		else {
-			end_idx++;
-			sum += end_idx;
+		else
+		{
+			sum += ++end_idx;
 		}
 	}
-	
+
 	cout << cnt << '\n';
 	
-	return 0;
+    return 0;
 }
