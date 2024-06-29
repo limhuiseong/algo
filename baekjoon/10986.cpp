e854f1385f839cf2d1f1c055cbdacfef599ecba5
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2024-06-29 07:18:21 +0000

    Refactor: Change vector to array

--- baekjoon/10986.cpp
+++ baekjoon/10986.cpp
@@ -1,36 +1,44 @@
+// (A + B) % C = ((A % C) + (B % C)) % C
+// if S[j] % M == S[i] % M
+// then (S[j] - S[i]) % M = 0
+
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int N, M;
+int N, M;
+long long A[1000001];
+long long S[1000001];
+
+long long C[1000001];
+long long ans = 0;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
 	cin >> N >> M;
-	
-	vector<long> S(N + 1, 0);
-	vector<long> C(M, 0);
-	
-	long cnt = 0;
-	
-	for (int i = 1; i <= N; i++) {
-		int tmp;
-		cin >> tmp;
-		S[i] = S[i - 1] + tmp;
-		
-		if (S[i] % M == 0) cnt++;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> A[i];
+		S[i] = S[i - 1] + A[i];
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (S[i] % M == 0) ans++;
 		C[S[i] % M]++;
 	}
-	
-	for (int i = 0; i < M; i++) {
+
+	for (int i = 0; i < M; i++)
+	{
 		if (C[i] > 1)
-			cnt += (C[i] * (C[i] - 1) / 2);
+			ans += C[i] * (C[i] - 1) / 2;
 	}
-	
-	cout << cnt << '\n';
-	
-	return 0;
+
+	cout << ans << '\n';
+    return 0;
 }
