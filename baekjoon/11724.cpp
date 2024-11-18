aac040e8edf61a095da2ee4f1a4cc7d571e6e75b
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2024-11-18 13:52:07 +0000

    Refactor: Modify baekjoon/11724.cpp

--- baekjoon/11724.cpp
+++ baekjoon/11724.cpp
@@ -3,47 +3,49 @@
 
 using namespace std;
 
+int N, M;
 static vector<vector<int>> A;
 static vector<bool> visited;
+int cnt = 0;
 
-void dfs(int x) {
-	if (visited[x]) return;
-	
-	visited[x] = true;
-	for (int i : A[x]) {
+void DFS(int v)
+{
+	if (visited[v])
+		return;
+	visited[v] = true;
+
+	for (int i : A[v])
+	{
 		if (!visited[i])
-			dfs(i);
+			DFS(i);
 	}
 }
 
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int N, M;
-	cin >> N >> M;
-	
+	cin >> N >> M; 
 	A.resize(N + 1);
 	visited = vector<bool>(N + 1, false);
-	
-	for (int i = 0; i < M; i++) {
+
+	for (int i = 0; i < M; i++)
+	{
 		int u, v;
 		cin >> u >> v;
-		
 		A[u].push_back(v);
 		A[v].push_back(u);
 	}
-	
-	int cnt = 0;
-	for (int i = 1; i <= N; i++) {
-		if (!visited[i]) {
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (!visited[i])
+		{
 			cnt++;
-			dfs(i);
+			DFS(i);
 		}
 	}
-	
-	cout << cnt << '\n';
-	
+
+	cout << cnt << "\n";
+
 	return 0;
 }
