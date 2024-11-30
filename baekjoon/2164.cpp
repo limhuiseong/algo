fd2d816667fd4ef25001ac24f055cdea69c1fb66
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2024-11-30 09:05:28 +0000

    Refactor: Introduce encapsulation

--- baekjoon/2164.cpp
+++ baekjoon/2164.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
 int N;
-queue<int> q;
+
+bool is_prime(int num)
+{
+    for (int i = 2; i <= num / 2; i++)
+        if (num % i == 0)
+            return false;
+    return true;
+}
+
+void dfs(int num, int place)
+{
+    if (N == place)
+    {
+        if (is_prime(num))
+            cout << num << "\n";
+        return;
+    }
+
+    for (int i = 1; i < 10; i++)
+    {
+        if (i % 2 == 0)
+            continue;
+        if (is_prime(num * 10 + i))
+            dfs(num * 10 + i, place + 1);
+    }
+}
+
+void solve()
+{
+    dfs(2, 1);
+    dfs(3, 1);
+    dfs(5, 1);
+    dfs(7, 1);
+}
 
 int main()
 {
@@ -14,17 +46,7 @@ int main()
 
     cin >> N;
 
-    for (int i = 1; i <= N; i++)
-    {
-        q.push(i);
-    }
+    solve();
 
-    while (q.size() != 1)
-    {
-        q.pop();
-        q.push(q.front());
-        q.pop();
-    }
-    cout << q.front() << '\n';
     return 0;
 }
